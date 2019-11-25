09b7c3ca4a29147920d70c080a1701145da6d74a
Parocq <55479321+Parocq@users.noreply.github.com>
2019-11-25 18:08:25 +0300

    Update MainActivity.java

--- app/src/main/java/com/german/stockapp/MainActivity.java
+++ app/src/main/java/com/german/stockapp/MainActivity.java
@@ -86,5 +86,12 @@ public class MainActivity extends AppCompatActivity {
 //
 //        DAOAuthorization daoAuthorization = new DAOAuthorization(db);
 //       // DAOAuthorization.insert(new Operator(text1, text2, text3));
+
+
+
+for(int i = 0; i < list.size; i++){
+if(list.get(i).getLogin().equals(textLogin) && list.get(i).getPass().equals){
+}
+}
     }
 }
